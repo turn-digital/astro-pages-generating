0234c404df515ac300645675cd44a7ef44f119e8
Vlad <thevlad2698@gmail.com>
2023-06-03 15:58:56 +0300

    adding base and site to conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
+
 export default defineConfig({
+    site: "https://turn-digital.github.io",
+    base: "/custom-domain-artis-check",
   integrations: [preact()]
 });
